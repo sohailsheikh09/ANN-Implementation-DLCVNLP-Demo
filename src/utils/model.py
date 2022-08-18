a36324cacaec8ce9fcf5897d085ee503f6c7d8dd
1234san <sana.quazi2405@gmail.com>
2022-08-18 13:57:12 +0530

    added a sava model function

--- src/utils/model.py
+++ src/utils/model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import time
+import os
 
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
-
     LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="inputLayer"),
               tf.keras.layers.Dense(
                   300, activation="relu", name="hiddenLayer1"),
@@ -15,7 +16,18 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     model_clf.summary()
 
     model_clf.compile(loss=LOSS_FUNCTION,
-                      optimizer=OPTIMIZER,
-                      metrics=METRICS)
+                      metrics=METRICS,
+                      optimizer=OPTIMIZER)
 
     return model_clf  # <<< untrained model
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
